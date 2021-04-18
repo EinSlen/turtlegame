f6be07dce6143fd282607fb552c4c9f47537b89d
DVLaD <62669119+EinSlen@users.noreply.github.com>
2021-04-18 15:35:08 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+def go_up():
+    if head.direction != "down":
+        head.direction = "up"
+ 
+def go_down():
+    if head.direction != "up":
+        head.direction = "down"
+ 
+def go_right():
+    if head.direction != "left":
+        head.direction = "right"
+ 
+def go_left():
+    if head.direction != "right":
+        head.direction = "left"
