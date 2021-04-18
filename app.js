db6df4352d08b51ba9c988c2346d9e629649db33
DVLaD <62669119+EinSlen@users.noreply.github.com>
2021-04-18 15:33:39 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+var getMessage = document.getElementById('time');
+var debut = 0;
+    do {  
+      debut + 1 
+    } while (true);
+  
+    
+    getMessage.innerHTML = debut +  ' msec';
+    console.log(debut + ' msec')
